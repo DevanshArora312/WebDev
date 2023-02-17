b7bb3cc1176dbb31815ede9f3fc83e456adf1c15
Devansh Arora <84780579+DevanshArora312@users.noreply.github.com>
2023-02-17 23:16:09 +0530

    updated script

--- Venatus/Public/Script/script.js
+++ Venatus/Public/Script/script.js
@@ -2,8 +2,12 @@
 function openRepo(){
   $('body > main > section > section:nth-child(2)').load("/Views/NavBar/Repositories.html")
 }
+function addSVG(){
+  $('body > main > section > section:nth-child(2) > div > span').load("/Views/Extra/svgfile.html");
+}
 function openPack(){
   $('body > main > section > section:nth-child(2)').load("/Views/NavBar/Packages.html")
+  
 }
 function openProj(){
   $('body > main > section > section:nth-child(2)').load("/Views/NavBar/Projects.html")
@@ -15,4 +19,5 @@ function openStars(){
 function openOver(){
   $('body > main > section > section:nth-child(2)').load("/Views/NavBar/Overview.html");
 }
+
 openOver()
