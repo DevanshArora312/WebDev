a675303d2eec20db982e108ad6bfb6617cbb4f3f
Devansh Arora <84780579+DevanshArora312@users.noreply.github.com>
2023-02-18 21:26:12 +0530

    updated script

--- Venatus/Public/Script/script.js
+++ Venatus/Public/Script/script.js
@@ -18,8 +18,70 @@ function openStars(){
 
 function openOver(){
   $('body > main > section > section:nth-child(2)').load("/Views/NavBar/Overview.html");
+  
 }
 function openRealRepo(){
   $('body > main > section > section:nth-child(2)').load("/Views/Extra/RealRepo.html");
+  
 }
+
+function dropClick1(){
+  var cls,style;
+  cls = document.querySelector('.repo-dropdown-content1');
+  style=getComputedStyle(cls);
+    if (style.display ==="none"){      
+      $('.repo-dropdown-content1').css("display" , "block");
+    }
+    else{
+    $('.repo-dropdown-content1').css("display" , "none");
+  }
+  }
+function dropClick2(){
+    var cls,style;
+    cls = document.querySelector('.repo-dropdown-content2');
+    style=getComputedStyle(cls);
+      if (style.display ==="none"){      
+        $('.repo-dropdown-content2').css("display" , "block");
+      }
+      else{
+      $('.repo-dropdown-content2').css("display" , "none");
+    }
+    }
+function dropClick3(){
+      var cls,style;
+      cls = document.querySelector('.repo-dropdown-content3');
+      style=getComputedStyle(cls);
+        if (style.display ==="none"){      
+          $('.repo-dropdown-content3').css("display" , "block");
+        }
+        else{
+        $('.repo-dropdown-content3').css("display" , "none");
+      }
+      }
+function hideContent1(){
+  $('.repo-dropdown-content1').css("display" , "none");
+}
+function hideContent2(){
+  $('.repo-dropdown-content2').css("display" , "none");
+}
+function hideContent3(){
+  $('.repo-dropdown-content3').css("display" , "none");
+}
+
 openOver()
+
+list = document.getElementsByClassName('nav-but');
+
+for (i=0;i<5;i++){
+    list[i].addEventListener("click",(e)=>{
+      elementID=e.target.id;
+      var elem = document.getElementById(elementID);
+  for (i=0;i<5;i++){
+    var obj = document.getElementById(tabs[i]);
+    obj.className = "nav-but";
+  }
+  elem.className = "nav-but2";
+    })
+}
+tabs = ['overview','repo','pack','proj','star'];
+
